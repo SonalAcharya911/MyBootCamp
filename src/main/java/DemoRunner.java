651352e3e44ee57d6c402a9dc1f17d5e789f9914
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-15 15:17:57 +0530

    checking maven

--- src/main/java/DemoRunner.java
+++
@@ -1,6 +0,0 @@
-public class DemoRunner {
-    public static void main(String[] args) {
-        System.out.println("Hiii");
-
-    }
-}
---
+++ src/main/java/com/xworkz/flounder/runner/DemoRunner.java
@@ -0,0 +1,7 @@
+package com.xworkz.flounder.runner;
+
+public class DemoRunner {
+    public void main1(String[] args) {
+        System.out.println("Hiii");
+    }
+}
