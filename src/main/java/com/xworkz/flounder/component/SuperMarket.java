9413cbc7db0026f6224c11c5d72d7332f5d40315
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-16 23:45:07 +0530

    checking maven

---
+++ src/main/java/com/xworkz/flounder/component/SuperMarket.java
@@ -0,0 +1,10 @@
+package com.xworkz.flounder.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SuperMarket {
+    public SuperMarket(){
+        System.out.println("created SuperMarket...");
+    }
+}
