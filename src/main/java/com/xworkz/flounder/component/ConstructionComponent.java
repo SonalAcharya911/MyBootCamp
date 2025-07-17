f1b1de344a5027a30fcffa86a21ae23b4ed47b35
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-17 23:46:14 +0530

    Component- instance creation using spring

---
+++ src/main/java/com/xworkz/flounder/component/ConstructionComponent.java
@@ -0,0 +1,10 @@
+package com.xworkz.flounder.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConstructionComponent {
+    public ConstructionComponent() {
+        System.out.println("created ConstructionComponent...");
+    }
+}
---
+++ src/main/java/com/xworkz/flounder/component/Cosmetic.java
@@ -0,0 +1,10 @@
+package com.xworkz.flounder.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cosmetic {
+    public Cosmetic() {
+        System.out.println("created Cosmetic...");
+    }
+}
