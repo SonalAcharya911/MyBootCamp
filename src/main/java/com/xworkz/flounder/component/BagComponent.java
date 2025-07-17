d01eb5b8264406ece6cb6426139b23f9ec64895b
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-17 23:45:48 +0530

    Component- instance creation using spring

---
+++ src/main/java/com/xworkz/flounder/component/BagComponent.java
@@ -0,0 +1,9 @@
+package com.xworkz.flounder.component;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BagComponent {
+    public BagComponent() {
+        System.out.println("created BagComponent...");
+    }
+}
