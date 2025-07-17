3053940c54c6f8c4f26371099a79eb81a987514a
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-17 23:45:55 +0530

    Component- instance creation using spring

--- src/main/java/com/xworkz/flounder/component/BookStoreComponent.java
+++ src/main/java/com/xworkz/flounder/component/BookStoreComponent.java
@@ -1,5 +1,8 @@
 package com.xworkz.flounder.component;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookStoreComponent{
     public BookStoreComponent(){
         System.out.println("created BookStoreComponent...");
