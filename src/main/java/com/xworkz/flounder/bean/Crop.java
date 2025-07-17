1e19cafb9e08e6f7ff6ec6ca4f49c81a9a5461b9
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-17 23:46:20 +0530

    Component- instance creation using spring

---
+++ src/main/java/com/xworkz/flounder/bean/Crop.java
@@ -0,0 +1,10 @@
+package com.xworkz.flounder.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Crop {
+    public Crop(){
+        System.out.println("created Crop...");
+    }
+}
