ca3b0c614ecd27266b9f635922139833c2afb907
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-17 23:45:39 +0530

    Component- instance creation using spring

---
+++ src/main/java/com/xworkz/flounder/component/Alcohol.java
@@ -0,0 +1,10 @@
+package com.xworkz.flounder.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alcohol {
+    public Alcohol() {
+        System.out.println("created Alcohol...");
+    }
+}
