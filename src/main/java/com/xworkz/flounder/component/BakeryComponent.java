638ab77b7360901b6cba8b25264a2fb278ca00df
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-18 23:57:55 +0530

    Component-instance creation using spring

--- src/main/java/com/xworkz/flounder/component/BakeryComponent.java
+++ src/main/java/com/xworkz/flounder/component/BakeryComponent.java
@@ -1,9 +1,15 @@
 package com.xworkz.flounder.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BakeryComponent {
+    @Autowired
+    @Qualifier("getFan")
+    private Fan fan;
+
     public BakeryComponent(){
         System.out.println("created BakeryComponent...");
     }
