e684fecea22dd7b2685b8957feb00b2c54008224
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-24 02:15:18 +0530

    controller in spring

---
+++ src/main/java/com/xworkz/flounder/dto/NameDTO.java
@@ -0,0 +1,16 @@
+package com.xworkz.flounder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NameDTO {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+}
